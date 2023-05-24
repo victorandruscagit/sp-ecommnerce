c797d33101e7b2ecec09b348021dbb421d523445
victorandruscagit <victor.v.andrusca@gmail.com>
2023-05-24 08:38:08 +0300

    First/added application.properities needed

---
+++ src/main/java/com/andrusca/ecommerce/SpEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.andrusca.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrusca/ecommerce/SpEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrusca.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

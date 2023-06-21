e685f0b1eed7b179a729c081b92de8c2656cc65c
victorandruscagit <victor.v.andrusca@gmail.com>
2023-06-21 13:21:25 +0300

    Developed country and state entities and CountryRepository

--- src/main/java/com/andrusca/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/andrusca/ecommerce/dao/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:63322/")
+@CrossOrigin("http://localhost:4200/")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
     Page<Product> findByNameContainingIgnoreCase(@Param("name") String  name, Pageable pageable);
---
+++ src/main/java/com/andrusca/ecommerce/entity/State.java
@@ -0,0 +1,24 @@
+package com.andrusca.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+}
